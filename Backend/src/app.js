90396251819f0c2a0d126ace88e68172127f8bf1
Ramiz1323 <reenabibi5678@gmail.com>
2026-03-08 02:05:43 +0530

    Cors error solved

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,7 +5,11 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
+
 app.use(express.json());
 app.use(cookieParser());
 
